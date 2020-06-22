61bdfc6788970737b28fc9f09ee9214b8ad24598
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-22 20:34:26 -0300

    codeclimate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import Table from './components/Table';
-import Filter from './components/Filter';
-import NumericFilter from './components/NumericFilter';
-import ListFilters from './components/ListFilters';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => 
-  <div className="App">
-    <span>StarWars Datatable with Filters</span>
-    <Filter />
-    <NumericFilter />
-    <ListFilters />
-    <Table />
-  </div>;
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
 
 export default App;

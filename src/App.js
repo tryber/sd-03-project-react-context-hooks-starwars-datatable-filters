590545a596dfae80ea587f9f036feb7431c93d0b
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-06-23 15:08:50 -0300

    Teste hooks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Home from './components/Home';
 import './App.css';
+import StarWarsProvider from './context/StarWarsProvider';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <StarWarsProvider>
+      <div className="App">
+        <Home />
+      </div>
+    </StarWarsProvider>
   );
 }
 

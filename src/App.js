6739e3cec7b079b55e77b869d4cd11f632ec324c
Jafet Henrique <jafetguerra6@gmail.com>
2020-06-23 19:56:12 -0300

    Projeto refatorado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import { useContext, useEffect } from 'react';
+import Table from './components/Table';
 import './App.css';
+import SWApiContext from './context/SWApiContext';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  // componentDidMount() {
+  //   const { apiRequestDispatch } = this.props;
+  //   apiRequestDispatch();
+  // }
+
+  const {
+    isLoading,
+    apiSWRequestFunction,
+  } = useContext(SWApiContext);
+
+  useEffect(() => {
+    apiSWRequestFunction();
+  }, []);
+
+  if (!isLoading) {
+    return (
+      <div className="App">
+        <Table />
+      </div>
+    );
+  }
+  return (<span>Loading...</span>);
+};
+
+// const mapDispatchToProps = (dispatch) => ({
+//   apiRequestDispatch: () => dispatch(getApiDataSw()),
+// });
+
+// const mapStateToProps = (state) => ({
+//   dataSw: state.apiSWReducer.data,
+//   isLoading: state.apiSWReducer.loading,
+// });
 
 export default App;
+
+App.propTypes = {
+  apiSWRequestFunction: PropTypes.func,
+  isLoading: PropTypes.bool,
+};
+
+App.defaultProps = {
+  apiSWRequestFunction: () => {},
+  isLoading: true,
+};
---
+++ src/context/SWApiContext.js
@@ -0,0 +1,7 @@
+import { createContext } from 'react';
+
+const SWApiContext = createContext();
+
+SWApiContext.displayName = 'SWApiContext';
+
+export default SWApiContext;

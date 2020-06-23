baa2b1df847a71b5c07fbbfbaa1fa1caf39e5a7b
Hfreitas <hfreitas86@gmail.com>
2020-06-22 22:52:40 -0300

    Alterações para atender code climate

--- src/components/filters/SortColumnsFilter.jsx
+++ src/components/filters/SortColumnsFilter.jsx
@@ -22,65 +22,65 @@ const initialState = {
   sort: '',
 };
 
+const renderColumnSelect = (callback, object) => (
+  <select
+    data-testid="column-sort"
+    onChange={(event) => callback({ ...object, column: event.target.value })}
+  >
+    {columns.map((column) => (
+      <option value={column.toLowerCase()} key={column}>
+        {column}
+      </option>
+    ))}
+  </select>
+);
+
+const renderSortSelect = (callback, object) => (
+  <div>
+    <label htmlFor="ASC">
+      ASC
+      <input
+        name="sort_radio"
+        type="radio"
+        data-testid="column-sort-input-asc"
+        value="ASC"
+        defaultChecked
+        onChange={(event) => callback({ ...object, sort: event.target.value })}
+      />
+    </label>
+    <label htmlFor="DESC">
+      DESC
+      <input
+        name="sort_radio"
+        type="radio"
+        data-testid="column-sort-input-desc"
+        value="DESC"
+        onChange={(event) => callback({ ...object, sort: event.target.value })}
+      />
+    </label>
+  </div>
+);
+
+const renderSubmitButton = (callback, object) => (
+  <button
+    type="button"
+    data-testid="column-sort-button"
+    onClick={() => callback({ ...object })}
+  >
+    Ordenar
+  </button>
+);
+
 function SortColumnsFilter() {
   const { setOrderFilter } = useContext(PlanetTableContext);
 
   const [sortFilter, setSortFilter] = useState({ ...initialState });
 
-  const renderColumnSelect = () => (
-    <select
-      data-testid="column-sort"
-      onChange={(event) => setSortFilter({ ...sortFilter, column: event.target.value })}
-    >
-      {columns.map((column) => (
-        <option value={column.toLowerCase()} key={column}>
-          {column}
-        </option>
-      ))}
-    </select>
-  );
-
-  const renderSortSelect = () => (
-    <div>
-      <label htmlFor="ASC">
-        ASC
-        <input
-          name="sort_radio"
-          type="radio"
-          data-testid="column-sort-input-asc"
-          value="ASC"
-          defaultChecked
-          onChange={(event) => setSortFilter({ ...sortFilter, sort: event.target.value })}
-        />
-      </label>
-      <label htmlFor="DESC">
-        DESC
-        <input
-          name="sort_radio"
-          type="radio"
-          data-testid="column-sort-input-desc"
-          value="DESC"
-          onChange={(event) => setSortFilter({ ...sortFilter, sort: event.target.value })}
-        />
-      </label>
-    </div>
-  );
-
-  const renderSubmitButton = () => (
-    <button
-      type="button"
-      data-testid="column-sort-button"
-      onClick={() => setOrderFilter({ ...sortFilter })}
-    >
-      Ordenar
-    </button>
-  );
-
   return (
     <div>
-      {renderColumnSelect()}
-      {renderSortSelect()}
-      {renderSubmitButton()}
+      {renderColumnSelect(setSortFilter, sortFilter)}
+      {renderSortSelect(setSortFilter, sortFilter)}
+      {renderSubmitButton(setOrderFilter, sortFilter)}
     </div>
   );
 }

3f66597265a292281c95137b293828dfed388bf6
Marco Barbosa <marco.meireles.b@gmail.com>
2020-06-23 14:55:11 -0300

    Mudando o Provider do Index para o App para passar nos testes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Provider from './context/Provider';
 import Table from './components/Table';
 import Filter from './components/Filter';
 import NumericFilter from './components/NumericFilter';
@@ -6,12 +7,14 @@ import ListFilters from './components/ListFilters';
 import './App.css';
 
 const App = () =>
-  <div className="App">
-    <span>StarWars Datatable with Filters</span>
-    <Filter />
-    <NumericFilter />
-    <ListFilters />
-    <Table />
-  </div>;
+  <Provider>
+    <div className="App">
+      <span>StarWars Datatable with Filters</span>
+      <Filter />
+      <NumericFilter />
+      <ListFilters />
+      <Table />
+  </div>
+  </Provider>;
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Provider from './context/Provider';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <Provider>
-    <App />
-  </Provider>,
+    <App />,
   document.getElementById('root'),
 );
 

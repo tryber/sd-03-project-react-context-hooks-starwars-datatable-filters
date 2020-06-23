2a4f28f4029561eef7fa8f9d06c21ca124cbde77
Rodrigo Brezolin <brezolinr@gmail.com>
2020-06-23 14:53:19 -0300

    all req

---
+++ src/components/HeaderTable.js
@@ -0,0 +1,10 @@
+ 
+import React, { useContext } from 'react';
+import StarWarsContext from '../context/StarWarsContext';
+
+const HeaderTable = () => {
+  const { filteredData } = useContext(StarWarsContext);
+  const heads = Object.keys(filteredData[0]);
+  return (<tr>{heads.map((head) => head !== 'residents' && <th key={head}>{head}</th>)}</tr>);
+};
+export default HeaderTable;
--- src/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import planetsList from './planetsList';
-import filters from './filters';
-
-const rootReducer = combineReducers({ planetsList, filters });
-
-export default rootReducer;
-// test CC

a24eaa1dd9c26ffa18e94fc32272ba076b0b086d
Hfreitas <hfreitas86@gmail.com>
2020-06-22 23:27:34 -0300

    Adequação do componente TableBody ao code climate

--- src/components/table/TableBody.jsx
+++ src/components/table/TableBody.jsx
@@ -5,11 +5,7 @@ import NoResultsTableBody from './NoResultsTableBody';
 
 const TableBody = () => {
   const { data, filters } = useContext(PlanetTableContext);
-  const {
-    filterByName: { name },
-    filterByNumericValues,
-    order: { column, sort },
-  } = filters;
+  const { filterByName: { name }, filterByNumericValues, order: { column, sort } } = filters;
   const filteredData = filterDataByNumericValue(
     data,
     name,
@@ -17,39 +13,43 @@ const TableBody = () => {
     sort,
     filterByNumericValues,
   );
-  return filteredData.length === 0 ? (
-    <NoResultsTableBody />
-  ) : (
-    <tbody>
-      {filteredData.map((planet) => (
-        <tr key={planet.name}>
-          <td data-testid="planet-name">{planet.name}</td>
-          <td>{planet.rotation_period}</td>
-          <td>{planet.orbital_period}</td>
-          <td>{planet.diameter}</td>
-          <td>{planet.climate}</td>
-          <td>{planet.gravity}</td>
-          <td>{planet.terrain}</td>
-          <td>{planet.surface_water}</td>
-          <td>{planet.population}</td>
-          <td>
-            {planet.films.map((film) => (
-              <a href={film} target="_blank" rel="noopener noreferrer" key={film}>
-                {film}
+  return filteredData.length === 0 ? (<NoResultsTableBody />)
+    : (
+      <tbody>
+        {filteredData.map((planet) => (
+          <tr key={planet.name}>
+            <td data-testid="planet-name">{planet.name}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.population}</td>
+            <td>
+              {planet.films.map((film) => (
+                <a
+                  href={film}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  key={film}
+                >
+                  {film}
+                </a>
+              ))}
+            </td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+            <td>
+              <a href={planet.url} target="_blank" rel="noopener noreferrer">
+                {planet.url}
               </a>
-            ))}
-          </td>
-          <td>{planet.created}</td>
-          <td>{planet.edited}</td>
-          <td>
-            <a href={planet.url} target="_blank" rel="noopener noreferrer">
-              {planet.url}
-            </a>
-          </td>
-        </tr>
-      ))}
-    </tbody>
-  );
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    );
 };
 
 export default TableBody;

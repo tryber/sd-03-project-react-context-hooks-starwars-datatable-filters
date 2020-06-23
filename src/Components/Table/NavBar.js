921eb70107a5f89ed29892c1aa95e4fe919af385
DiegoRafaelAF <draf0314@gmail.com>
2020-06-23 18:13:43 -0300

    Code Climate NavBar

--- src/Components/Table/NavBar.js
+++ src/Components/Table/NavBar.js
@@ -15,6 +15,40 @@ function filterColumn(valueFilter, option) {
   return !valueFilter.find(({ column }) => column === option);
 }
 
+function sideBar(handleChangeInput) {
+  return (
+    <header className="header">
+      <nav className="menu">
+        <select
+          data-testid="column-filter"
+          name="column"
+          onChange={(e) => handleChangeInput('column', e.target.value)}
+        >
+          <option value="" />
+          {columns.map((column) => (filterColumn(columns, column)
+            && (
+              <option key={column}>{column}</option>
+            )
+          ))}
+        </select>
+        <select
+          data-testid="comparison-filter"
+          name="comparison"
+          onChange={(e) => handleChangeInput('comparison', e.target.value)}
+        >
+          {comparisons.map((comparison) => <option key={comparison}>{comparison}</option>)}
+        </select>
+        <input
+          type="number"
+          data-testid="value-filter"
+          name="value"
+          onChange={(e) => handleChangeInput('value', e.target.value)}
+        />
+      </nav>
+    </header>
+  );
+}
+
 function NavBar() {
   const { saveFilter, filterSelect } = useContext(APIcontext);
   const [list, setList] = useState();
@@ -32,43 +66,9 @@ function NavBar() {
     });
   };
 
-  function sideBar() {
-    return (
-      <header className="header">
-        <nav className="menu">
-          <select
-            data-testid="column-filter"
-            name="column"
-            onChange={(e) => handleChangeInput('column', e.target.value)}
-          >
-            <option value="" />
-            {columns.map((column) => (filterColumn(columns, column)
-              && (
-                <option key={column}>{column}</option>
-              )
-            ))}
-          </select>
-          <select
-            data-testid="comparison-filter"
-            name="comparison"
-            onChange={(e) => handleChangeInput('comparison', e.target.value)}
-          >
-            {comparisons.map((comparison) => <option key={comparison}>{comparison}</option>)}
-          </select>
-          <input
-            type="number"
-            data-testid="value-filter"
-            name="value"
-            onChange={(e) => handleChangeInput('value', e.target.value)}
-          />
-        </nav>
-      </header>
-    );
-  }
-
   return (
     <div>
-      {sideBar()}
+      {sideBar(handleChangeInput, handleFormSubmit)}
       <div>
         <button
           type="button"

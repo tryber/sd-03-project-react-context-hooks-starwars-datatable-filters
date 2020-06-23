ecc3322ab4a1e5a2868db8cd77e16fc3cfd36132
luldelrey <rpgstrikered@gmail.com>
2020-06-23 10:31:30 -0300

    code cliamte

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
 import { StarWarsProvider } from './context/StarWarsContext';
-import TableRender from './components/RenderTable'
+import TableRender from './components/RenderTable';
 import './App.css';
 
-const App = () => {
-  return (
-    <div className="App">
-      <StarWarsProvider>
-        <TableRender />
-      </StarWarsProvider>
-    </div>
-  );
-};
+const App = () => (
+  <div className="App">
+    <StarWarsProvider>
+      <TableRender />
+    </StarWarsProvider>
+  </div>
+);
 
 export default App;

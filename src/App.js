29b3fee8e2d4744985a103b75b17a2b801868cdd
ilan274 <ilan.herbach@gmail.com>
2020-06-23 15:06:29 -0300

    Updating remote files - CC issues

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Home from './components/Home';
-import './App.css';
+import StarWarsProvider from './context/StarWarsProvider';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <StarWarsProvider>
+      <center>
+        <Home />
+      </center>
+    </StarWarsProvider>
   );
 }
 

6a98ac1a99b483a1f1b8e1e87fe2b1404da0b5a2
Cardoso <lsc.cardosoo@gmail.com>
2020-06-23 21:28:09 -0300

    cc

--- src/components/FilterList.js
+++ src/components/FilterList.js
@@ -5,16 +5,16 @@ function FilterList() {
   const { filters: { filterByNumericValues }, handleExcludeFilter } = useContext(FilterContext);
   return (
     <div>
-        {filterByNumericValues.map(({ column, comparison, value }) => (
-          <div data-testid="filter" key={column}>
-            <ul>
-              <li>column: {`${column}`}</li>
-              <li>comparison: {`${comparison}`}</li>
-              <li>value: {`${value}`}</li>
-            </ul>
-            <button onClick={() => handleExcludeFilter(column)}>X</button>
-          </div>
-        ))}
+      {filterByNumericValues.map(({ column, comparison, value }) => (
+        <div data-testid="filter" key={column}>
+          <ul>
+            <li>column: {`${column}`}</li>
+            <li>comparison: {`${comparison}`}</li>
+            <li>value: {`${value}`}</li>
+          </ul>
+          <button onClick={() => handleExcludeFilter(column)}>X</button>
+        </div>
+      ))}
     </div>
   );
 }

2d83fe3a60fd4bc45a8032c60059266b7f52cfdb
Wenderson belko <57719936+wendersonbelko@users.noreply.github.com>
2020-06-23 21:40:12 -0300

    teste

--- src/store/index.js
+++ src/store/index.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 
 import rootReducer from '../reducers';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window._REDUX_DEVTOOLS_EXTENSION_COMPOSE_ || compose;
 const store = createStore(rootReducer,
   composeEnhancers(applyMiddleware(thunk)));
 

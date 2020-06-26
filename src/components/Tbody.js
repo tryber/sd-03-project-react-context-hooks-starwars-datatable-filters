e30681ef570d44cd95ee20ba6b36a3db5f630af6
Rodrigo Militão <rodrigo_militao@hotmail.com.br>
2020-06-26 10:40:11 -0300

    last requisite

--- src/components/Tbody.js
+++ src/components/Tbody.js
@@ -5,7 +5,7 @@ const TableBody = ({ data }) => (
   <tbody>
     {data.map((planet) => (
       <tr key={planet.name}>
-        <td>{planet.name}</td>
+        <td data-testid="planet-name">{planet.name}</td>
         <td>{planet.rotation_period}</td>
         <td>{planet.orbital_period}</td>
         <td>{planet.diameter}</td>

3c1d9e0a8317ad60cdd3728118116a1085431526
DevT <aguiarthiag@gmail.com>
2020-06-27 18:59:58 -0300

    Fixing useEffect in App.js

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
           setData(payload.results);
         },
       );
-  }, []);
+  }, [setData]);
   return (
     <div className="App">
       <Filters />
--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-import Planet from './planet';
-import filters from './filter';
-
-const rootReducer = combineReducers({ Planet, filters });
-
-export default rootReducer;

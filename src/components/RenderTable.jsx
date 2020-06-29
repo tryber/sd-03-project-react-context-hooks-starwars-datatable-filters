a75f196f0c7c59ed2c12abf2ed690acc6f4f1a64
luldelrey <rpgstrikered@gmail.com>
2020-06-29 10:26:56 -0300

    component render table organized

--- src/components/RenderTable.jsx
+++ src/components/RenderTable.jsx
@@ -1,19 +1,24 @@
 import React, { useContext } from 'react';
 import { StarWarsContext } from '../context/StarWarsContext';
-import RenderTBody from './RenderTBody';
 import RenderTHead from './RenderTHead';
+import RenderTBody from './RenderTBody';
 import filterByNumeric from '../services/Filters';
 
 const TableRender = () => {
   const {
-    tableData: { dataTable, isRequesting },
+    tableData: {
+      dataTable,
+      isRequesting,
+    },
     filterData: { filters },
   } = useContext(StarWarsContext);
 
   if (isRequesting) return <h1>Loading ...</h1>;
 
   const filteredTable = filterByNumeric(
-    filters.filterByName.name, dataTable, filters.filterByNumericValues,
+    filters.filterByName.name,
+    dataTable,
+    filters.filterByNumericValues,
   );
 
   return (

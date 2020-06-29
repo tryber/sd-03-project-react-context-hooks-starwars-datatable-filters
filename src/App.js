69050413f576fecb561abdd6dd405cd6db528fe9
luldelrey <rpgstrikered@gmail.com>
2020-06-29 10:25:12 -0300

    numeric filters now render on app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { StarWarsProvider } from './context/StarWarsContext';
 import RenderFilterByName from './components/RenderFilterByName';
+import RenderNumericFilters from './components/RenderNumericFilters';
 import TableRender from './components/RenderTable';
 
 const App = () => (
   <div className="App">
     <StarWarsProvider>
       <RenderFilterByName />
+      <RenderNumericFilters />
       <TableRender />
     </StarWarsProvider>
   </div>

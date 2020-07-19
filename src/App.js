7d308377f7fc2c74e5668c7ff8d522be96599777
mirellaalves <mirella.alves@gmail.com>
2020-07-19 20:39:36 -0300

    starting context

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
 import React from 'react';
 import './App.css';
-
 import Table from './components/Table';
+import StarWarsContext from './context/StarWarsContext';
+
+const ShowContext = () => {
+  return (
+    <StarWarsContext.Consumer>
+      {(context) => {
+        return (
+          <span>{`O valor do contexto é ${JSON.stringify(context)}`}</span>
+        )
+      }}
+    </StarWarsContext.Consumer>
+  )
+}
 
 function App() {
+
+  const contextValue = {
+    isFetching: false,
+    data: [],
+  };
+
   return (
-    <div>
-      <Table />
-    </div>
+    <StarWarsContext.Provider value={contextValue}>
+      <div>
+        <ShowContext />
+        <Table />
+      </div>
+    </StarWarsContext.Provider>
   );
 }
 

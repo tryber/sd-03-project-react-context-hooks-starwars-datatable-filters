747980e6ec7f53ef13227e14b3bf57ee1fd7b1a3
RobertoAsgon <robertoasgon@gmail.com>
2020-07-19 21:54:57 -0400

    starting evaluation again

---
+++ src/components/Home.js
@@ -0,0 +1,29 @@
+import React, { useEffect, useContext } from 'react';
+import InputText from './InputText';
+import ByNumericValues from './ByNumericValues';
+import FilterOrder from './FilterOrder';
+import RemoveFilter from './RemoveFilter';
+import Table from './Table/Table';
+import StarWarsContext from '../context/StarWarsContext';
+
+function Home() {
+  const { requestAPI, isFetching } = useContext(StarWarsContext);
+  useEffect(() => {
+    requestAPI();
+  }, []);
+
+  if (isFetching) return <p>Loading...</p>;
+  
+  return (
+    <div>
+      <h1>StarWars Datatable with Filters</h1>
+      <InputText />
+      <ByNumericValues />
+      <FilterOrder />
+      <RemoveFilter />
+      <Table />
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/Table/Table.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import TableHeader from './TableHeader';
+import TableBody from './TableBody';
+
+function Table() {
+  return (
+    <div>
+      <table>
+        <TableHeader />
+        <TableBody />
+      </table>
+    </div >
+  );
+}
+
+export default Table;

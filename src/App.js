59c8811025066b3fea6ad1867dc433f20d5e1837
mirellaalves <mirella.alves@gmail.com>
2020-07-19 22:03:44 -0300

    trying context and correct testData file

--- src/App.js
+++ src/App.js
@@ -1,32 +1,87 @@
 import React from 'react';
 import './App.css';
-import Table from './components/Table';
+import Table2 from './components/Table2';
 import StarWarsContext from './context/StarWarsContext';
 
-const ShowContext = () => {
-  return (
-    <StarWarsContext.Consumer>
-      {(context) => {
-        return (
-          <span>{`O valor do contexto é ${JSON.stringify(context)}`}</span>
-        )
-      }}
-    </StarWarsContext.Consumer>
-  )
-}
+// const ShowContext = () => {
+//   return (
+//     <StarWarsContext.Consumer>
+//       {(context) => {
+//         return (
+//           <span>{`O valor do contexto é ${JSON.stringify(context)}`}</span>
+//         );
+//       }}
+//     </StarWarsContext.Consumer>
+//   );
+// };
 
 function App() {
-
   const contextValue = {
     isFetching: false,
-    data: [],
+    data: [
+      {
+          "name": "Tatooine", 
+          "rotation_period": "23", 
+          "orbital_period": "304", 
+          "diameter": "10465", 
+          "climate": "arid", 
+          "gravity": "1 standard", 
+          "terrain": "desert", 
+          "surface_water": "1", 
+          "population": "200000", 
+          "residents": [
+              "https://swapi-trybe.herokuapp.com/api/people/1/", 
+              "https://swapi-trybe.herokuapp.com/api/people/2/", 
+              "https://swapi-trybe.herokuapp.com/api/people/4/", 
+              "https://swapi-trybe.herokuapp.com/api/people/6/", 
+              "https://swapi-trybe.herokuapp.com/api/people/7/", 
+              "https://swapi-trybe.herokuapp.com/api/people/8/", 
+              "https://swapi-trybe.herokuapp.com/api/people/9/", 
+              "https://swapi-trybe.herokuapp.com/api/people/11/", 
+              "https://swapi-trybe.herokuapp.com/api/people/43/", 
+              "https://swapi-trybe.herokuapp.com/api/people/62/"
+          ], 
+          "films": [
+              "https://swapi-trybe.herokuapp.com/api/films/1/", 
+              "https://swapi-trybe.herokuapp.com/api/films/3/", 
+              "https://swapi-trybe.herokuapp.com/api/films/4/", 
+              "https://swapi-trybe.herokuapp.com/api/films/5/", 
+              "https://swapi-trybe.herokuapp.com/api/films/6/"
+          ], 
+          "created": "2014-12-09T13:50:49.641000Z", 
+          "edited": "2014-12-20T20:58:18.411000Z", 
+          "url": "https://swapi-trybe.herokuapp.com/api/planets/1/"
+      }, 
+      {
+          "name": "Alderaan", 
+          "rotation_period": "24", 
+          "orbital_period": "364", 
+          "diameter": "12500", 
+          "climate": "temperate", 
+          "gravity": "1 standard", 
+          "terrain": "grasslands, mountains", 
+          "surface_water": "40", 
+          "population": "2000000000", 
+          "residents": [
+              "https://swapi-trybe.herokuapp.com/api/people/5/", 
+              "https://swapi-trybe.herokuapp.com/api/people/68/", 
+              "https://swapi-trybe.herokuapp.com/api/people/81/"
+          ], 
+          "films": [
+              "https://swapi-trybe.herokuapp.com/api/films/1/", 
+              "https://swapi-trybe.herokuapp.com/api/films/6/"
+          ], 
+          "created": "2014-12-10T11:35:48.479000Z", 
+          "edited": "2014-12-20T20:58:18.420000Z", 
+          "url": "https://swapi-trybe.herokuapp.com/api/planets/2/"
+      }, ],
   };
 
   return (
     <StarWarsContext.Provider value={contextValue}>
       <div>
-        <ShowContext />
-        <Table />
+        {/* <ShowContext /> */}
+        <Table2 />
       </div>
     </StarWarsContext.Provider>
   );
--- src/components/Table2.js
+++ src/components/Table2.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 import { fetchStarWars } from '../actions';
 import TableHead from './TableHead';
-import Filters from './Filters';
+// import Filters from './Filters';
 import StarWarsContext from '../context/StarWarsContext';
 
 function switchComparison(column, comparison, value, planet) {
@@ -30,7 +30,7 @@ function descOrder(columnA, columnB) {
   return -1;
 }
 
-class Table extends Component {
+class Table2 extends Component {
 
   componentDidMount() {
     const { getStarWarsPlanetsData } = this.props;
@@ -79,16 +79,15 @@ class Table extends Component {
     return (
       <StarWarsContext.Consumer>
         {(context) => {
+          const { data } = context;
           return (
             <div>
               <h1 className="table-title" >StarWars Datatable with Filters</h1>
-              <Filters />
+              {/* <Filters /> */}
               <table>
                 <TableHead />
                 <tbody>
-                  {this.getFilteredValues()
-                  .sort((planetA, planetB) => this.sortPlanets(planetA, planetB))
-                  .map((planet) => (
+                  {data.map((planet) => (
                     <tr key={planet.name}>
                       <td>{planet.name}</td>
                       <td>{planet.rotation_period}</td>
@@ -126,7 +125,7 @@ const mapDispatchToProps = (dispatch) => ({
   getStarWarsPlanetsData: () => dispatch(fetchStarWars()),
 });
 
-Table.propTypes = {
+Table2.propTypes = {
   getStarWarsPlanetsData: PropTypes.func.isRequired,
   data: PropTypes.arrayOf(PropTypes.shape({
     name: PropTypes.string.isRequired,
@@ -159,9 +158,9 @@ Table.propTypes = {
   ).isRequired,
 };
 
-Table.defaultProps = {
+Table2.defaultProps = {
   data: null,
   name: null,
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(Table);
+export default connect(mapStateToProps, mapDispatchToProps)(Table2);

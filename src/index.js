bd1b15e47915121d0503faf129bafaf42813ef4f
mirellaalves <mirella.alves@gmail.com>
2020-07-21 23:45:21 -0300

    code climate issue

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <App />
+  <App />
 , document.getElementById('root'));
 
 serviceWorker.unregister();
